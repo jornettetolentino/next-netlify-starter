e8ce668fc8e8024d2458577b194b388780c6fdcb
jornettetolentino <jornettetolentino@gmail.com>
2025-10-22 16:23:33 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,14 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+    <script SameSite="None; Secure" type="module" src="https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs"></script>
+<div id="myLandbot" style="width: 100%; height: px"></div>
+<script type="module">
+  var myLandbot = new Landbot.Container({
+    container: '#myLandbot',
+    configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3192567-0J79WIUZTS6I14S7/index.json',
+  });
+</script>
       </Head>
 
       <main>
