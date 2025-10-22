a124cb75bf9f010f5e0a8653487e8504ac3cfcbe
jornettetolentino <jornettetolentino@gmail.com>
2025-10-22 16:26:01 +0800

    Update index.js
    embed landbot

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,13 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-    <script SameSite="None; Secure" type="module" src="https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs"></script>
-<div id="myLandbot" style="width: 100%; height: px"></div>
-<script type="module">
-  var myLandbot = new Landbot.Container({
-    container: '#myLandbot',
-    configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3192567-0J79WIUZTS6I14S7/index.json',
-  });
-</script>
+
+        {/* Load Landbot's external module script */}
+        <script
+          SameSite="None; Secure"
+          type="module"
+          src="https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs"
+        ></script>
       </Head>
 
       <main>
@@ -23,6 +22,25 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        {/* Landbot container */}
+        <div
+          id="myLandbot"
+          style={{ width: '100%', height: '600px' }} // Adjust height as needed
+        />
+
+        {/* Inline script to initialize Landbot */}
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var myLandbot = new Landbot.Container({
+                container: '#myLandbot',
+                configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3192567-0J79WIUZTS6I14S7/index.json'
+              });
+            `,
+          }}
+        />
       </main>
 
       <Footer />
