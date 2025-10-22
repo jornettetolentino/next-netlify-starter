e68a95f10f68a33be55157a70f77772ea102add8
jornettetolentino <jornettetolentino@gmail.com>
2025-10-22 17:27:01 +0800

    Update index.js
    updating the landbot pop up

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -8,52 +9,44 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Load Landbot's external module script */}
-        <script
-          SameSite="None; Secure"
-          type="module"
-          src="https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs"
-        ></script>
       </Head>
 
+      {/* Load the Landbot script asynchronously */}
+      <Script
+        src="https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs"
+        type="module"
+        strategy="lazyOnload"
+      />
+
+      {/* Inline script to initialize Landbot with event listeners */}
+      <Script strategy="lazyOnload" id="landbot-init">
+        {`
+          window.addEventListener('mouseover', initLandbot, { once: true });
+          window.addEventListener('touchstart', initLandbot, { once: true });
+          var myLandbot;
+          function initLandbot() {
+            if (!myLandbot) {
+              var s = document.createElement('script');
+              s.type = "module"
+              s.async = true;
+              s.addEventListener('load', function() {
+                myLandbot = new Landbot.Popup({
+                  configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3192567-0J79WIUZTS6I14S7/index.json',
+                });
+              });
+              s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+              var x = document.getElementsByTagName('script')[0];
+              x.parentNode.insertBefore(s, x);
+            }
+          }
+        `}
+      </Script>
+
       <main>
-        <Header title="Welcome to Synrgy Global AI demo!" />
+        <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-
-        {/* Landbot container */}
-        <div
-          id="myLandbot"
-          style={{ width: '40%', height: '500px' }} // Adjust height as needed
-        />
-
-        {/* Updated inline script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.addEventListener('mouseover', initLandbot, { once: true });
-              window.addEventListener('touchstart', initLandbot, { once: true });
-              var myLandbot;
-              function initLandbot() {
-                if (!myLandbot) {
-                  var s = document.createElement('script');
-                  s.type = "module"
-                  s.async = true;
-                  s.addEventListener('load', function() {
-                    myLandbot = new Landbot.Popup({
-                      configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3192567-0J79WIUZTS6I14S7/index.json',
-                    });
-                  });
-                  s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
-                  var x = document.getElementsByTagName('script')[0];
-                  x.parentNode.insertBefore(s, x);
-                }
-              }
-            `,
-          }}
-        />
       </main>
 
       <Footer />
